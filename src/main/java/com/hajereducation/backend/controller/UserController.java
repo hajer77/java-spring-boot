1de8bae51435c8111f6a2f678d90bba47dac2681
Hajer Dahech <hajeriefst@gmail.com>
2022-10-06 20:09:42 +0100

    remove  local connection with react app CrossOrigin

--- src/main/java/com/hajereducation/backend/controller/UserController.java
+++ src/main/java/com/hajereducation/backend/controller/UserController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+/* For local connection with react app*/
+/*  @CrossOrigin("http://localhost:3000") */
 
 public class UserController {
     @Autowired
